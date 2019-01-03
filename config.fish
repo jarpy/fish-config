39e4b41aee6ea81cce24dd80a87363054dc724b6
Toby McLaughlin <toby@jarpy.net>
2019-01-04 10:00:38 +1100

    Add direnv, ssh-agent logic, and auto X11

--- config.fish
+++ config.fish
@@ -10,3 +10,15 @@ set -x __fish_git_prompt_showupstream true
 set -x __fish_git_prompt_show_informative_status true
 
 set -x fish_color_valid_path
+
+eval (direnv hook fish)
+
+alias vi=vim
+
+ps -eo comm,user | egrep -q '^ssh-agent.+jarpy$'; or ssh-agent
+ln -sf (find /tmp/ssh-* -user $USER -name 'agent.*' 2>/dev/null) $HOME/.ssh/ssh_auth_sock
+set -x SSH_AUTH_SOCK $HOME/.ssh/ssh_auth_sock
+
+if test "$XDG_VTNR" = "1" -a "$TERM" = "linux"
+  exec startx
+end
