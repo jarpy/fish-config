31bea7b679d900478c354e8ad5807a7185b2016f
Toby McLaughlin <toby@jarpy.net>
2019-01-14 10:13:51 +1100

    Emacs tweaks

--- config.fish
+++ config.fish
@@ -17,10 +17,15 @@ eval (direnv hook fish)
 
 alias vi=vim
 alias e='emacsclient --no-wait'
+set -x EDITOR 'emacsclient'
 
 ps -eo comm,user | egrep -q '^ssh-agent.+jarpy$'; or ssh-agent
 ln -sf (find /tmp/ssh-* -user $USER -name 'agent.*' 2>/dev/null) $HOME/.ssh/ssh_auth_sock
 set -x SSH_AUTH_SOCK $HOME/.ssh/ssh_auth_sock
+
+# Emacs ansi-term fix
+# REF: https://github.com/fish-shell/fish-shell/issues/1907
+function fish_title; end
 
 if test "$XDG_VTNR" = "1" -a "$TERM" = "linux"
   exec startx
